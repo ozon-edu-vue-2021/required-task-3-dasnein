7fbeff4379ecdbe032f6d709e17a1a6fdccc5502
Oleg Moskalev <sank.mail@gmail.com>
2021-11-02 18:21:24 +0300

    implement bootstrap from workshop

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,55 +1,111 @@
 <template>
-    <div class="map">
-        <h3>Карта офиса</h3>
+  <div class="map">
+    <h3>Карта офиса</h3>
 
-        <div
-            v-if="!isLoading"
-            class="map-root"
-        >
-            <!-- map -->
-        </div>
-        <div v-else>Loading...</div>
+    <div v-if="!isLoading" class="map-root">
+      <MapSVG ref="svg" />
+      <TableSVG ref="table" v-show="false" />
     </div>
+    <div v-else>Loading...</div>
+  </div>
 </template>
 
 <script>
+import * as d3 from "d3";
+
+import MapSVG from "@/assets/images/map.svg";
+import TableSVG from "@/assets/images/workPlace.svg";
+
+import tables from "@/assets/data/tables.json";
+import legend from "@/assets/data/legend.json";
+
 export default {
-    data() {
-        return {
-            isLoading: false,
-        };
+  components: {
+    MapSVG,
+    TableSVG,
+  },
+
+  data() {
+    return {
+      isLoading: false,
+
+      svg: null,
+      group: null,
+
+      tables,
+      tableSVG: null,
+    };
+  },
+
+  methods: {
+    drawTables() {
+      const svgTablesGroup = this.group
+        .append("g")
+        .classed("groupPlaces", true);
+
+      this.tables.map((table) => {
+        const svgTable = svgTablesGroup
+          .append("g")
+          .attr("transform", `translate(${table.x} ${table.y}) scale(0.5)`)
+          .attr("id", table._id)
+          .classed("employer-place", true);
+
+        svgTable
+          .append("g")
+          .attr("transform", `rotate(${table.rotate || 0})`)
+          .attr("group_id", table.group_id)
+          .html(this.tableSVG.html())
+          .attr(
+            "fill",
+            legend.find((group) => group.group_id === table.group_id)?.color ??
+              "transparent"
+          );
+      });
     },
+  },
+
+  mounted() {
+    this.svg = d3.select(this.$refs.svg);
+    this.group = this.svg.select("g");
+    this.tableSVG = d3.select(this.$refs.table);
+
+    if (this.group) {
+      this.drawTables();
+    } else {
+      console.log("ERROR");
+    }
+  },
 };
 </script>
 
 <style scoped>
 .map {
-    height: 100%;
-    width: 100%;
-    padding: 24px;
-    overflow: hidden;
-    box-sizing: border-box;
-    display: flex;
-    flex-direction: column;
+  height: 100%;
+  width: 100%;
+  padding: 24px;
+  overflow: hidden;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
 }
 
 .map-root {
-    height: 100%;
-    width: 100%;
-    overflow: hidden;
-    box-sizing: border-box;
+  height: 100%;
+  width: 100%;
+  overflow: hidden;
+  box-sizing: border-box;
 }
 
 h3 {
-    margin-top: 0px;
+  margin-top: 0px;
 }
 
 ::v-deep svg {
-    height: 100%;
-    width: 100%;
+  height: 100%;
+  width: 100%;
 }
 
 ::v-deep .table {
-    cursor: pointer;
+  cursor: pointer;
 }
 </style>
